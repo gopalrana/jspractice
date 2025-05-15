ca909c8548bd2b28d41907f59bae6837c4f553f4
gopalrana <rajputgopal321@gmail.com>
2025-05-15 23:38:39 +0530

    first commit

---
+++ 12-05-25/13-05-25/arr.js
@@ -0,0 +1,51 @@
+// // let arr = [1,4,5,6,7,8,8,4,5];
+// // let newarr = arr[0]
+// // for (let i = 0; i < arr.length; i++) {
+// //      let element = arr[i]
+// //     if (element>newarr) {
+// //         newarr = element
+// //     }   
+// // }
+// // console.log(newarr);
+
+
+// function palidrome_num(arr) {
+
+//     for (let i = arr; i >.length; i++) {
+//         const element = array[i];
+        
+//     }
+    
+// }
+
+
+
+
+
+
+// // function getHighest(arr) {
+// //     if (arr.length === 0) return null;
+ 
+// //     let max = arr[0];
+ 
+// //     for (let i = 1; i < arr.length; i++) {
+// //         if (arr[i] > max) {
+// //             max = arr[i];
+// //         }
+// //     }
+ 
+// //     return max;
+// // }
+
+
+function palidrome(arr) {
+
+    revarr = arr.split('').reverse().join('');
+
+    if (revarr === arr) {
+        return "Palidrome";
+   
+    }    
+}
+
+console.log(palidrome[1,1,1]);
---
+++ 12-05-25/13-05-25/pa.js
@@ -0,0 +1,20 @@
+let  n=5;
+for (let i = 0; i < n; i++) {
+      row ='';
+    for (let j = i; j <n ; j++) {
+            row +='*';
+
+
+        // if (i === 0 || i===n-1 || j===0 || j===n-1 ) {
+        //     row +='*';
+            
+        // }
+        // else{
+        //     row+=' '
+        // }
+    }
+    console.log(row);
+    
+
+    
+}
